12baf76f47fcb9f7c51beb8c2b86a22cb077dad8
mmore500 <mmore500.login@gmail.com>
2026-05-16 20:44:25 +0000

    Deploying to bindle-2024-10-20-example.py from @ mmore500/gptp2026@58494fbc4f4d0273f8d0f48ffdc5a14526409c4b 🚀

--- bindle/2026-05-16-perf-quality.py
+++ bindle/2026-05-16-perf-quality.py
@@ -41,20 +41,6 @@ def import_pkg():
     )
 
 
-@app.cell
-def _():
-    from conduitpylib.viz import _get_defaults as cfg
-
-    from conduitpylib.viz import beleaguerment_facetplot, beleaguerment_regplot
-
-    from conduitpylib.wrangle import (
-        retrieve_and_prepare_delta_dataframes,
-        wrangle_instrumentation_longform,
-    )
-
-    return
-
-
 @app.cell(hide_code=True)
 def do_watermark(mo, watermark):
     mo.md(
@@ -138,6 +124,37 @@ def _(cliffs_delta, ds_proc, pd, scipy_stats):
         ["asynchronicity mode", "executable"]
     ):
         _g1, _g2 = (
+            _group.loc[_group["ncpus"] == 1, "Update Walltime (ms)"],
+            _group.loc[_group["ncpus"] == 64, "Update Walltime (ms)"],
+        )
+
+        _res.append(
+            {
+                "mode": _mode,
+                "exec": _exec,
+                "n": len(_group),
+                "%": 100 * (_g2.mean() / _g1.mean() - 1),
+                **dict(
+                    zip(
+                        ["wstat", "p"],
+                        scipy_stats.wilcoxon(_g1, _g2, alternative="less"),
+                    )
+                ),
+                **dict(zip(["delta", "interp"], cliffs_delta(_g1, _g2))),
+            },
+        )
+
+    pd.DataFrame(_res)
+    return (filtered_procs,)
+
+
+@app.cell
+def _(cliffs_delta, filtered_procs, pd, scipy_stats):
+    _res = []
+    for (_mode, _exec), _group in filtered_procs.groupby(
+        ["asynchronicity mode", "executable"]
+    ):
+        _g1, _g2 = (
             _group.loc[_group["ncpus"] == 16, "Update Walltime (ms)"],
             _group.loc[_group["ncpus"] == 64, "Update Walltime (ms)"],
         )
@@ -148,13 +165,18 @@ def _(cliffs_delta, ds_proc, pd, scipy_stats):
                 "exec": _exec,
                 "n": len(_group),
                 "%": 100 * (_g2.mean() / _g1.mean() - 1),
-                **dict(zip(["kstat", "p"], scipy_stats.kruskal(_g1, _g2))),
+                **dict(
+                    zip(
+                        ["wstat", "p"],
+                        scipy_stats.wilcoxon(_g1, _g2, alternative="less"),
+                    )
+                ),
                 **dict(zip(["delta", "interp"], cliffs_delta(_g1, _g2))),
             },
         )
 
     pd.DataFrame(_res)
-    return (filtered_procs,)
+    return
 
 
 @app.cell
@@ -174,7 +196,12 @@ def _(cliffs_delta, filtered_procs, pd, scipy_stats):
                 "exec": _exec,
                 "n": len(_group),
                 "%": 100 * (_g2.mean() / _g1.mean() - 1),
-                **dict(zip(["kstat", "p"], scipy_stats.kruskal(_g1, _g2))),
+                **dict(
+                    zip(
+                        ["wstat", "p"],
+                        scipy_stats.wilcoxon(_g1, _g2, alternative="less"),
+                    )
+                ),
                 **dict(zip(["delta", "interp"], cliffs_delta(_g1, _g2))),
             },
         )
@@ -184,7 +211,7 @@ def _(cliffs_delta, filtered_procs, pd, scipy_stats):
 
 
 @app.cell
-def _(filtered_procs, mpl, np, pathlib, plt, sns, tp):
+def _(filtered_procs, mpl, np, pathlib, plt, sns, target_y, tp, va):
     with tp.teed(
         sns.relplot,
         data=filtered_procs,
@@ -206,7 +233,12 @@ def _(filtered_procs, mpl, np, pathlib, plt, sns, tp):
         for _i, (_ax, (_signif1, _signif2)) in enumerate(
             zip(
                 _g.axes.flat,
-                [("*", "n.s."), ("***", "*"), ("***", "***"), ("***", "***")],
+                [
+                    ("**", "n.s."),
+                    ("***", "n.s."),
+                    ("***", "***"),
+                    ("***", "***"),
+                ],
             )
         ):
             for line in _ax.lines:
@@ -215,40 +247,40 @@ def _(filtered_procs, mpl, np, pathlib, plt, sns, tp):
                     _x, _y.min(), _y, color=line.get_color(), alpha=0.2
                 )
 
-                target_y = max(_y.max(), _y.min() * 1.3, _y.min() + 0.7)
-                _ax.hlines(target_y, 1, 64, color="gray", ls=":", alpha=0.5)
+                _target_y = max(_y.max(), _y.min() * 1.3, _y.min() + 0.7)
+                _ax.hlines(_, 1, 64, color="gray", ls=":", alpha=0.5)
 
-                pct_change = ((_y[3] - _y[0]) / abs(_y[0])) * 100
-                va = {True: "bottom", False: "top"}[
+                _pct_change = ((_y[3] - _y[0]) / abs(_y[0])) * 100
+                _va = {True: "bottom", False: "top"}[
                     target_y < np.mean(_ax.get_ylim())
                 ]
                 _ax.text(
                     x=1,
                     y=target_y
                     + np.ptp(_ax.get_ylim())
-                    * {"bottom": 0.05, "top": -0.05}[va],
-                    s=f"|+{pct_change:.0f}%{_signif1}",
+                    * {"bottom": 0.05, "top": -0.05}[_va],
+                    s=f"|+{_pct_change:.0f}%{_signif1}",
                     alpha=1.0,
                     color="gray",
                     ha="left",
                     fontsize=9,
-                    va=va,
+                    va=_va,
                 )
 
-                pct_change = ((_y[3] - _y[2]) / abs(_y[2])) * 100
-                va = "bottom"
+                _pct_change = ((_y[3] - _y[2]) / abs(_y[2])) * 100
+                _va = "bottom"
                 # _ax.hlines(target_y + np.ptp(_ax.get_ylim()) * 0, 16, 64, color="beige", ls="--")
                 _ax.text(
                     x=16,
                     y=target_y
                     + np.ptp(_ax.get_ylim())
                     * {"bottom": 0.05, "top": -0.05}[va],
-                    s=f"|+{pct_change:.0f}%{_signif2}",
+                    s=f"|+{_pct_change:.0f}%{_signif2}",
                     alpha=1.0,
                     color="gray",
                     ha="left",
                     fontsize=9,
-                    va=va,
+                    va=_va,
                 )
 
         _g.map_dataframe(
@@ -280,6 +312,172 @@ def _(filtered_procs, mpl, np, pathlib, plt, sns, tp):
                         0: "Digital Evo Walltime (ns)",
                         1: "Graph Color Walltime (ns)",
                     }[i],
+                    fontsize=11,
+                )
+
+        _g.axes[0, 0].set_ylabel("Best\nEffort")
+        _g.axes[1, 0].set_ylabel("Global\nSync")
+    return
+
+
+@app.cell
+def _(cliffs_delta, filtered_procs, pd, scipy_stats):
+    _res = []
+    for (_mode, _exec), _group in filtered_procs.groupby(
+        ["asynchronicity mode", "executable"]
+    ):
+        _g1, _g2 = (
+            _group.loc[_group["ncpus"] == 1, "conflicts per cpu"],
+            _group.loc[_group["ncpus"] == 64, "conflicts per cpu"],
+        )
+
+        _res.append(
+            {
+                "mode": _mode,
+                "exec": _exec,
+                "n": len(_group),
+                "%": 100 * (_g2.mean() / _g1.mean() - 1),
+                **dict(
+                    zip(
+                        ["wstat", "p"],
+                        scipy_stats.wilcoxon(_g1, _g2, alternative="less"),
+                    )
+                ),
+                **dict(zip(["delta", "interp"], cliffs_delta(_g1, _g2))),
+            },
+        )
+
+    pd.DataFrame(_res)
+    return
+
+
+@app.cell
+def _(cliffs_delta, filtered_procs, pd, scipy_stats):
+    _res = []
+    for (_mode, _exec), _group in filtered_procs.groupby(
+        ["asynchronicity mode", "executable"]
+    ):
+        _g1, _g2 = (
+            _group.loc[_group["ncpus"] == 16, "conflicts per cpu"],
+            _group.loc[_group["ncpus"] == 64, "conflicts per cpu"],
+        )
+
+        _res.append(
+            {
+                "mode": _mode,
+                "exec": _exec,
+                "n": len(_group),
+                "%": 100 * (_g2.mean() / _g1.mean() - 1),
+                **dict(
+                    zip(
+                        ["wstat", "p"],
+                        scipy_stats.wilcoxon(_g1, _g2, alternative="less"),
+                    )
+                ),
+                **dict(zip(["delta", "interp"], cliffs_delta(_g1, _g2))),
+            },
+        )
+
+    pd.DataFrame(_res)
+    return
+
+
+@app.cell
+def _(filtered_procs, mpl, np, pathlib, plt, sns, tp):
+    with tp.teed(
+        sns.relplot,
+        data=filtered_procs,
+        x="ncpus",
+        y="conflicts per cpu",
+        col="executable",
+        col_order=["channel_selection"],
+        hue="executable",
+        row="asynchronicity mode",
+        row_order=[3, 0],
+        err_style=None,
+        facet_kws=dict(margin_titles=True, sharey="col"),
+        kind="line",
+        legend=False,
+        teeplot_show=True,
+        teeplot_subdir=pathlib.Path(__file__).stem,
+    ) as _g:
+
+        # adapted from https://github.com/mwaskom/seaborn/issues/2410#issuecomment-753474050
+        for _i, (_ax, (_signif1, _signif2)) in enumerate(
+            zip(_g.axes.flat, [("n.s.", "n.s."), ("***", "***")])
+        ):
+            for _line in _ax.lines:
+                _x, _y = _line.get_xydata().T
+                _ax.fill_between(
+                    _x, _y.min(), _y, color=_line.get_color(), alpha=0.2
+                )
+
+                _target_y = max(_y.max(), _y.min() + 10)
+                print(_target_y)
+                _ax.hlines(_target_y, 1, 64, color="gray", ls=":", alpha=0.5)
+
+                _pct_change = ((_y[3] - _y[0]) / abs(_y[0])) * 100
+                _va = {True: "bottom", False: "top"}[
+                    _target_y < np.mean(_ax.get_ylim())
+                ]
+                print(np.ptp(_ax.get_ylim()))
+                _ax.text(
+                    x=1,
+                    y=_target_y
+                    + np.ptp(_ax.get_ylim())
+                    * {"bottom": 0.05, "top": -0.05}[_va],
+                    s=f"|+{_pct_change:.0f}%{_signif1}",
+                    alpha=1.0,
+                    color="gray",
+                    ha="left",
+                    fontsize=9,
+                    va=_va,
+                )
+
+                _pct_change = ((_y[3] - _y[2]) / abs(_y[2])) * 100
+                _va = "bottom"
+                # _ax.hlines(target_y + np.ptp(_ax.get_ylim()) * 0, 16, 64, color="beige", ls="--")
+                _ax.text(
+                    x=16,
+                    y=_target_y
+                    + np.ptp(_ax.get_ylim())
+                    * {"bottom": 0.05, "top": -0.05}[_va],
+                    s=f"|+{_pct_change:.0f}%{_signif2}",
+                    alpha=1.0,
+                    color="gray",
+                    ha="left",
+                    fontsize=9,
+                    va=_va,
+                )
+
+        _g.map_dataframe(
+            sns.lineplot,
+            x="ncpus",
+            y="conflicts per cpu",
+            color="k",
+            errorbar="sd",
+            err_style="bars",
+            legend=False,
+            lw=0,
+        )
+
+        _g.set(ylim=(0, None))
+        _g.set(xscale="log")
+        _g.figure.set_size_inches(6, 2)
+        _g.set_titles(col_template="{col_name}", row_template="")
+        plt.subplots_adjust(hspace=0.3)
+
+        for _i, _ax in enumerate(_g.axes.flat):
+            _ax.minorticks_off()
+            _ax.get_xaxis().set_major_formatter(mpl.ticker.ScalarFormatter())
+            _ax.set_xticks([1, 4, 16, 64])
+            if _i == 0:
+                sns.despine(ax=_ax, bottom=True)
+            if _i == 0:
+                _ax.set_title(
+                    {
+                        0: "Graph Color Soln Quality",
+                    }[_i],
                     fontsize=11,
                 )
 
