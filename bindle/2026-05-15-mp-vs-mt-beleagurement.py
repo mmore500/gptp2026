b269ba75126ca4826b754be4a205a1b323bb15e8
mmore500 <mmore500.login@gmail.com>
2026-05-16 14:52:12 +0000

    Deploying to bindle-2024-10-20-example.py from @ mmore500/gptp2026@336758426163e2dd7c35a54778f3ec8d5500d698 🚀

--- bindle/2026-05-15-mp-vs-mt-beleagurement.py
+++ bindle/2026-05-15-mp-vs-mt-beleagurement.py
@@ -294,7 +294,7 @@ def _(df_long, palette, pathlib, plt, sns, tp):
         x="Type",
         y="Count Per Second",
         col="Multiprocessing",
-        col_order=["dummy", "Internode"],
+        col_order=["", "Internode"],
         style="indexx",
         hue="Hostname",
         alpha=0.5,
