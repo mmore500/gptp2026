707426a433daed64048f2459938477a28476e6e4
mmore500 <mmore500.login@gmail.com>
2026-05-16 23:05:41 +0000

    Deploying to bindle-2024-10-20-example.py from @ mmore500/gptp2026@758acb1c790ad2d216b7e4e4fbfec646c3686888 🚀

--- bindle/2026-05-15-mp-vs-mt-beleagurement-ts.py
+++ bindle/2026-05-15-mp-vs-mt-beleagurement-ts.py
@@ -57,10 +57,10 @@ def delimit_prep_data(mo):
 
 @app.cell
 def _(requests):
-    with open("/tmp/4ys9v", "wb") as fp:
+    with open("/tmp/aum7w", "wb") as fp:
         fp.write(
             requests.get(
-                "https://osf.io/download/4ys9v", allow_redirects=True
+                "https://osf.io/download/aum7w", allow_redirects=True
             ).content,
         )
     return
@@ -68,7 +68,7 @@ def _(requests):
 
 @app.cell
 def _(pd):
-    data = pd.read_csv("/tmp/4ys9v", compression="gzip")
+    data = pd.read_csv("/tmp/aum7w", compression="gzip")
     data
     return (data,)
 
