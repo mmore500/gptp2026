d400970e1fe034a168f2f6d1b6bcaffa9dcfefaf
mmore500 <mmore500@users.noreply.github.com>
2026-05-22 21:51:18 +0000

    Deploying to bindle-2026-05-16-weak-scaling.py from @ mmore500/gptp2026@40fe70cb894b09b6ab9bdc5b11ef7ce301852210 🚀

--- bindle/2026-05-16-perf-quality.py
+++ bindle/2026-05-16-perf-quality.py
@@ -897,7 +897,7 @@ def _(filtered_procs, mpl, pathlib, pd, sns, tp):
                     _ax.text(
                         _x[-1],
                         _y[-1] + 0.04 * _lines_y[0].max(),
-                        "best effort",
+                        "best\neffort",
                         ha="right",
                         va="bottom",
                         color=_green,
@@ -999,6 +999,7 @@ def _(filtered_procs, mpl, pathlib, pd, sns, tp):
                 color=_eb_color,
                 errorbar="sd",
                 err_style="bars",
+                err_kws={"capsize": 3},
                 legend=False,
                 lw=0,
             )
