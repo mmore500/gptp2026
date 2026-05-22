94a14af6de5480f2256894c893c5205d4bd1de00
mmore500 <mmore500.login@gmail.com>
2026-05-22 23:13:23 +0000

    Deploying to bindle-2026-05-16-perf-quality.py from @ mmore500/gptp2026@1acac5ec7c8581d1c4b4e02e87e8419fb1290303 🚀

--- bindle/2026-05-15-mp-vs-mt-beleagurement.py
+++ bindle/2026-05-15-mp-vs-mt-beleagurement.py
@@ -380,6 +380,8 @@ def _(data, mo, palette, pathlib, plt, scipy_stats, sns, tp):
 
     for x in [
         _pivot,
+        _pivot.mean(),
+        _pivot.std(),
         scipy_stats.wilcoxon(_pivot["lac-221"], _pivot["lac-220"]),
     ]:
         mo.output.append(x)
@@ -423,6 +425,8 @@ def _(data, mo, palette, pathlib, plt, scipy_stats, sns, tp):
 
     for _x in [
         _pivot2,
+        _pivot2.mean(),
+        _pivot2.std(),
         scipy_stats.wilcoxon(_pivot2["lac-221"], _pivot2["lac-220"]),
     ]:
         mo.output.append(_x)
